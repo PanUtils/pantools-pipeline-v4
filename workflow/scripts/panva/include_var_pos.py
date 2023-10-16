d527684bbf152aae12e302d8825c294a03ced292
robin <roel.vanesch@wur.nl>
2023-10-16 16:00:38 +0200

    commented out print statement

--- workflow/scripts/panva/include_var_pos.py
+++ workflow/scripts/panva/include_var_pos.py
@@ -41,7 +41,7 @@ def create_var_pos_df(single_id, panva_p, df_all_pos, pheno=None, version='old')
         # print(hom_id)
 
         if "Informative" not in df_var_pos.columns:
-            print(hom_id)
+            #print(hom_id)
             df_all_pos.to_csv(os.path.join(hom_id_out, 'alignments.csv'), index=False)
         else:
             df_var_pos = df_var_pos.rename(columns={"Position": "position", "Informative": "informative"})
