0275a121afe72a00e4211997bcd2bad77ea2ff81
robin <roel.vanesch@wur.nl>
2023-10-27 11:45:41 +0200

    fixed syntax of snakemake parameter in homology_selection.sh

--- workflow/scripts/homology_selection.sh
+++ workflow/scripts/homology_selection.sh
@@ -5,5 +5,5 @@ while IFS= read -r line
 do
   grep "$line" ${snakemake_input[all_groups]} | sed 's/:.*$//g' >> temp_homology_groups.txt
 done <  ${snakemake_input[gene_selection]}
-tr '\n' ',' < temp_homology_groups.txt > {snakemake_output[0]}
+tr '\n' ',' < temp_homology_groups.txt > ${snakemake_output[0]}
 rm temp_homology_groups.txt
